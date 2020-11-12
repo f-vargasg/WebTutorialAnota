8a82fc9ba67b98bcadfb5974170911b2e26998f7
Francisco Vargas G. <52474105+f-vargasg@users.noreply.github.com>
2020-11-11 22:46:18 -0600

    Se añadió un comentario

--- src/java/bean/Usuario.java
+++ src/java/bean/Usuario.java
@@ -3,9 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package bean;
 
 import java.sql.Date;
+
+// Esto es un cometario adicional
 
 /**
  *
